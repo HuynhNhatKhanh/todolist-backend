0a2dbb1ac80a200a9740a8372b6eaf439ea1b59c
Khanh <khanhhuynh28082000@gmail.com>
2025-02-25 10:01:16 +0700

    feat: [Todo-List] Update config

--- config/cors.go
+++ config/cors.go
@@ -14,8 +14,8 @@ import (
 // The preflight request is cached for 12 hours.
 func CorsConfig() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		// AllowOrigins:     []string{"http://localhost:5173", "https://yourdomain.com"},
-		AllowOrigins:     []string{"*"},
+		AllowOrigins:     []string{"http://localhost:5173", "https://todolist-frontend-ten.vercel.app/"},
+		// AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Content-Type", "Authorization", "X-Requested-With"},
 		AllowCredentials: true,
